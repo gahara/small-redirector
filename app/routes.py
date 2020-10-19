82be57319ab6a877040e9baf7efcd703bbfe49c0
Madina Alieva <gahara@yandex-team.ru>
2020-10-19 20:15:48 +0300

    fixed path

--- app/routes.py
+++ app/routes.py
@@ -6,4 +6,4 @@ from app import app
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def index(path):
-    return redirect(f'http://google.com/{path}', code=302)
+    return redirect(f'http://google.com{request.full_path}', code=302)
