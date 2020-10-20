89952348361bba3dbf1a6e67888fe913c5eae806
gahara <gahara@MacBook-Pro-gahara.local>
2020-10-20 19:12:01 +0300

    redone request

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
-from flask import  redirect, request
+import os
+import requests
+from flask import request, Response
 from flask_cors import CORS
+from dotenv import load_dotenv
 from app import app
+
+load_dotenv()
+HOST = os.environ.get('HOST')
 
 HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 cors = CORS(app)
@@ -8,4 +14,16 @@ cors = CORS(app)
 @app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
 @app.route('/<path:path>', methods=HTTP_METHODS)
 def catch_all(path):
-    return redirect(f'http://google.com{request.full_path}', code=302)
+    resp = requests.request(
+        method=request.method,
+        url=request.url.replace(request.host_url, HOST),
+        headers={key: value for (key, value) in request.headers if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()]
+
+    response = Response(resp.content, resp.status_code, headers)
+
+    return response
