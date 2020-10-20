935667e57e5ee1e2b4bccd10d41ddaf5ebb9669b
Madina Alieva <gahara@yandex-team.ru>
2020-10-20 13:44:34 +0300

    allow all methods

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,9 @@ from flask import  redirect
 
 from app import app
 
+HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
+@app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
+@app.route('/<path:path>', methods=HTTP_METHODS)
 def index(path):
     return redirect(f'http://google.com{request.full_path}', code=302)
