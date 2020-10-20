1a571ce193286862faad614b63da456a705d4c59
Madina Alieva <gahara@yandex-team.ru>
2020-10-20 14:32:20 +0300

    attempt to fix cors

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,12 @@
 from flask import  redirect
-
+from flask_cors import CORS, cross_origin
 from app import app
 
 HTTP_METHODS = ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'CONNECT', 'OPTIONS', 'TRACE', 'PATCH']
+cors = CORS(app)
 
 @app.route('/', defaults={'path': ''}, methods=HTTP_METHODS)
 @app.route('/<path:path>', methods=HTTP_METHODS)
+@cross_origin
 def index(path):
     return redirect(f'http://google.com{request.full_path}', code=302)
